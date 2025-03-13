939244345cdf61e4f94bb994f4502310737c91bb
Helen <huanghelen30@gmail.com>
2025-03-13 19:22:46 -0400

    create page components & add bgimg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OasPage from "./pages/OasPage/OasPage";
+import OgfPage from "./pages/OgfPage/OgfPage";
+import './App.scss'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<OasPage />}></Route>
+        <Route path="/ogf" element={<OgfPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
