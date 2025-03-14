5240ea7ff805960d87e683e757cb2740693e367b
FelciaPreethi <felciapreethi96@gmail.com>
2025-03-14 09:09:23 -0700

    Add close button

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import OasPage from "./pages/OasPage/OasPage";
 import OgfPage from "./pages/OgfPage/OgfPage";
 import "./App.scss";
-import Widget from "./components/Widget/Widget";
 
 function App() {
   return (
     <BrowserRouter>
-      <Widget />
       <Routes>
         <Route path="/oas/:persona" element={<OasPage />}></Route>
         <Route path="/ogf/:persona" element={<OgfPage />}></Route>
--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,6 +1,6 @@
 import "./widget.scss";
 import responses from "../../data/responses.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Widget({ pageName, persona }) {
   const [initial, setInitial] = useState(true);
@@ -8,16 +8,19 @@ function Widget({ pageName, persona }) {
   const [transparency, setTransparency] = useState(true);
   const [closeWidget, setCloseWidget] = useState(false);
 
-  
   function handleWidgetClose(event) {
-    setCloseWidget(!closeWidget);
-    setExpand(false);
-    setTransparency(true);
-    setTimeout(() => setCloseWidget(false), 300);
+    setCloseWidget(true);
+  }
+
+  function handleOpen() {
+    setCloseWidget(false);
   }
 
   function handleExpand(event) {
     setExpand(!expand);
+    if (expand) {
+      setCloseWidget(false);
+    }
   }
 
   function handleOpacity(event) {
@@ -27,87 +30,99 @@ function Widget({ pageName, persona }) {
     }
   }
 
-  function getSectionClassName() {
-    let classname = "content";
-    if (expand) {
-      classname += " content__fullview";
-    }
-    if (closeWidget) {
-      classname += " content--hide";  
-    }
-    if (!closeWidget && !expand) {
-      classname += " content--transparent";
-    }
-    return classname;
-  }
-
-  function getCtaClassName() {
-    let ctaClass = "cta";
-    if (expand) {
-      ctaClass += " cta__fullview";
-    } else {
-      ctaClass += " cta--transparent";
-    }
-
-    if (closeWidget) {
-      ctaClass += " cta--hidden";
-    }
-
-    return ctaClass;
-  }
+  useEffect(() => {}, [closeWidget]);
 
   function formatSummary(summary) {
-    const points = summary.split(/\s(?=\d+\.)/); 
-  
+    const points = summary.split(/\s(?=\d+\.)/);
+
     return points.map((point, index) => (
-      <li key={index} className="summary-item">{point}</li> //
+      <li key={index} className="summary-item">
+        {point}
+      </li> //
     ));
   }
 
-  return (
-    <div className={expand ? "widget widget__fullview" : "widget"}>
-      <svg 
-        className={getCtaClassName()}
-        width="200" 
-        height="35" 
-        viewBox="0 0 200 30" 
-        fill="none" 
+  return closeWidget ? (
+    <svg
+      className={"cta__open"}
+      width="200"
+      height="35"
+      viewBox="0 0 200 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={handleOpen}
+      style={{ cursor: "pointer" }}
+    >
+      <g filter="url(#filter0_d_26_4)">
+        <rect x="4" width="326" height="55" rx="17.5" fill="#C864A6" />
+      </g>
+      <rect x="8.5" y="4.5" width="60" height="26" rx="13" stroke="white" />
+      <path
+        d="M14 18H61V16H14L20 10L18 8L10 16L18 24L20 22L14 18Z"
+        fill="white"
+      />
+    </svg>
+  ) : (
+    <div
+      className={`widget${expand ? " widget__fullview" : ""}${
+        closeWidget ? " widget__close" : ""
+      }`}
+    >
+      <svg
+        className={`cta${expand ? " cta__fullview" : " cta--transparent"}`}
+        width="200"
+        height="35"
+        viewBox="0 0 200 30"
+        fill="none"
         xmlns="http://www.w3.org/2000/svg"
         onClick={handleExpand}
         style={{ cursor: "pointer" }}
       >
         <g filter="url(#filter0_d_26_4)">
-          <rect x="4" width="326" height="55" rx="17.5" fill="#C864A6"/>
+          <rect x="4" width="326" height="55" rx="17.5" fill="#C864A6" />
         </g>
-        <rect x="8.5" y="4.5" width="60" height="26" rx="13" stroke="white"/>
+        <rect x="8.5" y="4.5" width="60" height="26" rx="13" stroke="white" />
         <path
-          d={expand
-            ? "M61 18H14V16H61L55 10L57 8L65 16L57 24L55 22L61 18Z" 
-            : "M14 18H61V16H14L20 10L18 8L10 16L18 24L20 22L14 18Z"} 
+          d={
+            expand
+              ? "M61 18H14V16H61L55 10L57 8L65 16L57 24L55 22L61 18Z"
+              : "M14 18H61V16H14L20 10L18 8L10 16L18 24L20 22L14 18Z"
+          }
           fill="white"
         />
       </svg>
 
-      <section className={getSectionClassName()} onClick={handleOpacity}>
+      <section
+        className={`content${expand ? " content__fullview" : ""}${
+          !expand ? " content--transparent" : ""
+        }`}
+        onClick={handleOpacity}
+      >
+        <div className="close__button" onClick={handleWidgetClose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 50 50"
+            width="25px"
+            height="25px"
+          >
+            <path d="M 11.5 11 C 11.372 11 11.243984 11.048984 11.146484 11.146484 C 11.049484 11.244484 11 11.372 11 11.5 C 11 11.628 11.048484 11.755516 11.146484 11.853516 L 24.292969 25 L 11.146484 38.146484 C 10.951484 38.341484 10.951484 38.658516 11.146484 38.853516 C 11.244484 38.950516 11.372 39 11.5 39 C 11.628 39 11.755516 38.951516 11.853516 38.853516 L 25 25.707031 L 38.146484 38.853516 C 38.341484 39.048516 38.658516 39.048516 38.853516 38.853516 C 39.048516 38.657516 39.049516 38.342484 38.853516 38.146484 L 25.707031 25 L 38.853516 11.853516 C 39.048516 11.658516 39.048516 11.341484 38.853516 11.146484 C 38.657516 10.951484 38.342484 10.950484 38.146484 11.146484 L 25 24.292969 L 11.853516 11.146484 C 11.756016 11.048984 11.628 11 11.5 11 z" />
+          </svg>
+        </div>
         <div className="content__expand" onClick={handleExpand}>
           <div>
             {responses.map((item, index) => {
               return item.page === pageName &&
                 item.persona_id === parseInt(persona) ? (
-                  <div key={index}>
-                  {expand ? ( 
+                <div key={index}>
+                  {expand ? (
                     <div className="summary">
                       <p>
-                        <strong>What you need to know:
-                          </strong> 
-                          {formatSummary(item.personalized_summary)}
+                        <strong>What you need to know:</strong>
+                        {formatSummary(item.personalized_summary)}
                       </p>
                       <p>
-                        <strong>Next Steps:
-                          </strong> 
-                          <div className="next-steps">
-                            {item.next_steps}
-                          </div>
+                        <strong>Next Steps:</strong>
+                        <div className="next-steps">{item.next_steps}</div>
                       </p>
                     </div>
                   ) : (
@@ -115,7 +130,7 @@ function Widget({ pageName, persona }) {
                       {item.summarized_summary}
                     </div>
                   )}
-                  </div>
+                </div>
               ) : (
                 ""
               );
