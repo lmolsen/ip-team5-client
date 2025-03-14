7f21ef66c88b56f927c33da122aee5d3811f6a5e
Helen <huanghelen30@gmail.com>
2025-03-13 23:30:30 -0400

    add simple styling

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -40,31 +40,44 @@ function Widget({ pageName, persona }) {
 
   return (
     <div className={expand ? "widget widget__fullview" : "widget"}>
+      <svg 
+        className="cta" 
+        width="200" 
+        height="30" 
+        viewBox="0 0 200 30" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={handleExpand}
+        style={{ cursor: "pointer" }}
+      >
+        <g filter="url(#filter0_d_26_4)">
+          <rect x="4" width="326" height="35" rx="17.5" fill="#C864A6"/>
+        </g>
+        <rect x="8.5" y="4.5" width="60" height="26" rx="13" stroke="white"/>
+        <path
+          d={expand
+            ? "M61 18H14V16H61L55 10L57 8L65 16L57 24L55 22L61 18Z" 
+            : "M14 18H61V16H14L20 10L18 8L10 16L18 24L20 22L14 18Z"} 
+          fill="white"
+        />
+      </svg>
+
       <section className={getSectionClassName()} onClick={handleOpacity}>
         <div className="content__expand" onClick={handleExpand}>
-          <svg fill="#000000" height="24" width="24" viewBox="0 0 54 54">
-            <g>
-              <path d="M0,0v54h54V0H0z M52,52H2V2h50V52z" />
-              <path
-                d="M7,8.414l15.293,15.293C22.488,23.902,22.744,24,23,24s0.512-0.098,0.707-0.293c0.391-0.391,0.391-1.023,0-1.414L8.414,7
-		h11.729V5H5v15.143h2V8.414z"
-              />
-              <path
-                d="M45.586,47H33.857v2H49V33.857h-2v11.729L31.707,30.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414L45.586,47z
-		"
-              />
-            </g>
-          </svg>
           <div>
             {responses.map((item, index) => {
               return item.page === pageName &&
                 item.persona_id === parseInt(persona) ? (
                 <div key={index} className="summary">
                   <p>
-                    <strong>Summary:</strong> {item.personalized_summary}
+                    <strong>Summary:
+                      </strong> 
+                      {item.personalized_summary}
                   </p>
                   <p>
-                    <strong>Next Steps:</strong> {item.next_steps}
+                    <strong>Next Steps:
+                      </strong> 
+                      {item.next_steps}
                   </p>
                 </div>
               ) : (
